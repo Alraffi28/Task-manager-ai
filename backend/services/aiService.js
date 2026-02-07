54f2a93acc9d8398f9049cf5fe2d7ba5f0c2f635
Alraffi28 <mohammedalraffi@gmail.com>
2026-02-07 20:33:09 +0530

    Added AI logic

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -1,15 +1,16 @@
 const axios = require("axios");
+const AI_URL = "https://task-manager-ai-1.onrender.com/predict"
 
 exports.classifyTask = async (description) => {
+  
+    const attemptRequest = async()=>{
+      return await axios.post(AI_URL, {description},{timeout:2000})
+    }
   try {
-    const response = await axios.post(
-      "https://task-manager-ai-1.onrender.com/predict",
-      { description },
-      {timeout : 2000}
-    );
-    return response.data;
+    return (await attemptRequest()).data
   } catch (error) {
-    console.error("AI SERVICE ERROR : ",error.response?.data || error.message)
-    throw new Error("AI Service Error");
+    console.log("AI asleep");
+    await new Promise(resolve => setTimeout(resolve, 5000))
+    return (await attemptRequest()).data
   }
 };
