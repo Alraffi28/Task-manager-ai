6956f107113bcb3adf59d68585d235a1b35185a7
Alraffi28 <mohammedalraffi@gmail.com>
2026-02-07 20:57:06 +0530

    Added Flask warm-up call before classification

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -1,16 +1,19 @@
 const axios = require("axios");
-const AI_URL = "https://task-manager-ai-1.onrender.com/predict"
+const AI_BASE = "https://task-manager-ai-1.onrender.com";
 
 exports.classifyTask = async (description) => {
-  
-    const attemptRequest = async()=>{
-      return await axios.post(AI_URL, {description},{timeout:2000})
-    }
   try {
-    return (await attemptRequest()).data
+    await axios.get(AI_BASE);
+    const response = await axios.post(
+      `${AI_BASE}/predict`,
+      { description },
+      { timeout: 20000 }
+    );
+
+    return response.data;
+
   } catch (error) {
-    console.log("AI asleep");
-    await new Promise(resolve => setTimeout(resolve, 5000))
-    return (await attemptRequest()).data
+    console.error("AI Service Error:", error.message);
+    throw new Error("AI Service Error");
   }
 };
