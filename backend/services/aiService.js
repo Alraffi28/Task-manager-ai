8ce8d1db38359e9a36dc877a375be2c2eedec8f9
Alraffi28 <mohammedalraffi@gmail.com>
2026-02-07 17:23:59 +0530

    UI improvement

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -3,12 +3,13 @@ const axios = require("axios");
 exports.classifyTask = async (description) => {
   try {
     const response = await axios.post(
-      "https://task-manager-ai-1.onrender.com",
-      { description }
+      "https://task-manager-ai-1.onrender.com/predict",
+      { description },
+      {timeout : 2000}
     );
-
     return response.data;
   } catch (error) {
+    console.error("AI SERVICE ERROR : ",error.response?.data || error.message)
     throw new Error("AI Service Error");
   }
 };
