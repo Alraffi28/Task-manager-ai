ac951d3c9633c8338beddb8b6e1e76efe286e1bf
Alraffi28 <mohammedalraffi@gmail.com>
2026-02-07 15:27:13 +0530

    Frontend ready for deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://task-manager-ai-wda4.onrender.com/api/tasks"
 });
 
 export default API;
