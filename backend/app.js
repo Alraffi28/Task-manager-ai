1944db93be8f17239f4891d3b3727c2fcfc17b14
Alraffi28 <mohammedalraffi@gmail.com>
2026-03-23 10:20:13 +0530

    error handle

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-
+require('dotenv').config()
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin : process.env.CLIENT_URL}));
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
+
+app.use((err , res , req)=>{
+    console.log(err.stack);
+    res.status(500).json({message : "Internal server error"})
+});
 
 module.exports = app;
